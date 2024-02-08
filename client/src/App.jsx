c01fb97fb01e2b0a9f3c2109ff32db2ff01011f0
Kumar Ayush <140546983+Kumar-AyushD@users.noreply.github.com>
2024-02-08 06:06:07 +0530

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <h1 className="text-3xl text-red-400">App</h1>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
