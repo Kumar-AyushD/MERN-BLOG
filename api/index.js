51e8d6bbf02d05e5c2b583e774f76cf26bdb1542
Kumar Ayush <140546983+Kumar-AyushD@users.noreply.github.com>
2024-02-09 13:15:45 +0530

    Footer

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -14,7 +16,21 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001..");
+app.listen(3002, () => {
+  console.log("Server is running on port 3002..");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
