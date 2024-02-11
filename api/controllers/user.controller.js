d584a314502589147dcbeb3c1dca247cf36ce285
Kumar Ayush <140546983+Kumar-AyushD@users.noreply.github.com>
2024-02-11 13:48:42 +0530

    Updat user profile

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,56 @@
-const test = (req, res)=>{
-  res.json({message: 'API is working'});
-}
+import errorHandler from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 
-export default test;
+export const test = (req, res) => {
+  res.json({ message: "API is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update the user"));
+  }
+
+  if (req.params.password) {
+    if (req.params.password.length < 6) {
+      next(errorHandler(400, "Password must be greater tan 6 characters"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 to 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    // if (req.body.username != req.body.username.tolowerCase) {
+    //   return next(errorHandler(400, "Username must be in lowercase only"));
+    // }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letter and numbers")
+      );
+    }
+  }
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,32 +1,201 @@
-import { Button, TextInput } from "flowbite-react";
-import { useSelector } from "react-redux";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase.js";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../redux/user/userSlice.js";
 
 export default function DashProfile() {
+  const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
+  const [imgFile, setImgFile] = useState(null);
+  const [imgFilUrl, setImgFileUrl] = useState(null);
+  const [imgFileUploadingProgress, setImgFileUploadingProgress] =
+    useState(null);
+  const [imgFileUploadError, setImgFileUploadError] = useState(null);
+  const [imgFileUploading, setImgFileUploading] = useState(false);
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImgFile(file);
+      setImgFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imgFile) {
+      uploadImage();
+    }
+  }, [imgFile]);
+
+  const uploadImage = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 2*1024*1024 &&
+    //       request.resource.contentType.matches('image/.*');
+    //     }
+    //   }
+    // }
+    setImgFileUploading(true);
+    setImgFileUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imgFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imgFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImgFileUploadingProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImgFileUploadError(
+          "Could not upload image (File must be less than 2MB"
+        );
+        setImgFileUploadingProgress(null);
+        setImgFile(null);
+        setImgFileUrl(null);
+        setImgFileUploading(false);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgFileUrl(downloadURL);
+          setFormData({ ...formData, profilePicture: downloadURL });
+          setImgFileUploading(false);
+        });
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setUpdateUserError(null);
+    setUpdateUserSuccess(null);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError("No Changes made");
+      return;
+    }
+    if (imgFileUploading) {
+      setUpdateUserError("Please Wait for image to upload");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(data.message);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
-      <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full "
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imgFileUploadingProgress && (
+            <CircularProgressbar
+              value={imgFileUploadingProgress || 0}
+              text={`${imgFileUploadingProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imgFileUploadingProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imgFilUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full border-8 border-[lightgray]"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imgFileUploadingProgress &&
+              imgFileUploadingProgress < 100 &&
+              "opacity-60"
+            }`}
           />
         </div>
+        {imgFileUploadError && (
+          <Alert color="failure">{imgFileUploadError}</Alert>
+        )}
+
         <TextInput
           type="text"
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
+          onChange={handleChange}
         />
         <TextInput
           type="email"
           id="email"
           placeholder="Email"
           defaultValue={currentUser.email}
+          onChange={handleChange}
         />
-        <TextInput type="password" id="password" placeholder="password" />
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
         <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
@@ -35,6 +204,8 @@ export default function DashProfile() {
         <span className="cursor-pointer">Delete Account</span>
         <span className="cursor-pointer">Sign Out</span>
       </div>
+      {updateUserSuccess && <Alert color="success" className="mt-5"></Alert>}
+      {updateUserError && <Alert color="failure" className="mt-5"></Alert>}
     </div>
   );
 }
